0800b14fed4b5f0986a49ffb820c5e751a3b1175
RimasBerz <116100732+RimasBerz@users.noreply.github.com>
2023-02-25 19:28:27 +0200

    Add files via upload

---
+++ EfContext/Department.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Sales.EfContext
+{
+    public class Department
+    {
+        public Guid Id { get; set; }                 // Набор свойств сущности в точности
+        public String Name { get; set; } = null!;    // повторяет структуру таблицы
+        public List<Manager> Managers { get; set; }
+
+        public List<Manager> PastWorkers { get; set; }
+        public String ToShortString()
+        {
+            return Id.ToString()[..4] + "... " + Name;
+        }
+    }
+}
---
+++ EfContext/Manager.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Sales.EfContext
+{
+    public class Manager
+    { 
+        public Guid Id { get; set; }
+        public String Surname { get; set; } = null!;
+        public String Name { get; set; } = null!;
+        public String SecName { get; set; } = null!;
+        public Guid Id_main_dep { get; set; }
+        public Guid? Id_sec_dep { get; set; }
+        public Guid? Id_chief { get; set; }
+        public Department MainDep { get; set; }
+
+        public Department? SecDep { get; set; }
+
+        public Manager? Chief { get; set; }
+        public List<Manager> Subordinates { get; set; }
+        public String ToShortString()
+        {
+            return $"{Id.ToString()[..4]}...  {Surname} {Name[0]}. {SecName[0]}.";
+        }
+    
+    }
+}
